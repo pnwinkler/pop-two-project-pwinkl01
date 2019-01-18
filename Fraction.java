c3a567cfbab6739d3ae77a596d0be69d8b2e9d5d
pnwinkler <pwinkl01@dcs.bbk.ac.uk>
2019-01-18 15:26:11 +0000

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -18,40 +18,12 @@ interface Fraction extends Comparable<Fraction> {
 
     Fraction inverse();
 
-    /**
-     * Returns <pre>true</pre> if <pre>o</pre> is a <pre>fraction</pre> equal to <pre>this</pre>,
-     * and <pre>false</pre> in all other cases.
-     * @param o the object to compare this one to
-     * @return whether the true fractions are equal
-     */
     @Override
     boolean equals(Object o);
 
-    /**
-     * Returns:
-     * <ul>
-     *     <li>A negative <pre>int</pre> if <pre>this</pre> is less than <pre>o</pre>.</li>
-     *     <li>Zero if <pre>this</pre> is equal to <pre>o</pre>.</li>
-     *     <li>A positive <pre>int</pre> if <pre>this</pre> is greater than <pre>o</pre>.</li>
-     * </ul>
-     *
-     * @param f the fraction to compare <pre>this</pre> to
-     * @return the result of the comparison
-     */
     @Override
     int compareTo(Fraction f);
-//    int compareTo(Fraction f);
 
-    /**
-     * Returns a <pre>String</pre> of the form <pre>n/d</pre>, where <pre>n</pre> is the
-     * <em>numerator</em> and <pre>d</pre> is the <em>denominator</em>.
-     * However, if <pre>d</pre> is <pre>1</pre>, just return <pre>n</pre> (as a <pre>String</pre>).
-     *
-     * The returned <pre>String</pre> should not contain any blanks.
-     * If the fraction represents a negative number, a minus sign should precede <pre>n</pre>
-     *
-     * @return the string representation fo the fraction
-     */
     @Override
     String toString();
 }
--- FractionImpl.java
+++ FractionImpl.java
@@ -1,22 +1,23 @@
-// todo: all methods should have comments. Follow the instructions at the bottom of the instructions page
-// todo: see to it that fractions are immutable after creation
 package fraction;
 
 public class FractionImpl implements Fraction {
     private int numerator;
     private int denominator;
 
+    /** constructor: creates fraction from numerator, denominator */
     public FractionImpl(int numerator, int denominator) {
         int[] result = simplify_fraction(numerator, denominator);
         this.numerator = result[0];
         this.denominator = result[1];
     }
 
+    /** constructor: creates fraction from wholenumber */
     public FractionImpl(int wholeNumber) {
         this.numerator = wholeNumber;
         this.denominator = 1;
     }
 
+    /** constructor: creates fraction from strings such as "4" or "-6/2" */
     public FractionImpl(String fraction) {
         // fraction contains either whole number like "-3" or fraction like "8/-12"
         if (!fraction.contains("/")) {
@@ -32,10 +33,12 @@ public class FractionImpl implements Fraction {
         }
     }
 
+    /**
+     * for internal use only (not for users).
+     * takes a string fraction then return its numerator and denominator
+     * does not simplify
+     * assumes it is receiving a fraction, not a wholenumber */
     private int[] fraction_string_to_ints(String str_fraction) {
-        // take a string fraction then return its nom and denom
-        // does not simplify
-        // assumes it is receiving a fraction, not a wholenumber
         int slash_index = str_fraction.indexOf("/");
         String s_numr = str_fraction.substring(0, slash_index);
         String s_denom = str_fraction.substring(slash_index + 1);
@@ -48,9 +51,13 @@ public class FractionImpl implements Fraction {
         return numer_denom2;
     }
 
+    /**
+     * internal use only
+     * simplifies fraction
+     * accepts int numerator and int denominator
+     * returns array containing the simplified int numerator and int denominator
+     * throws ArithmeticException if denominator equals 0 */
     private int[] simplify_fraction(int numerator, int denominator) {
-        // interact with local_numer and local_denom so that
-        // this function can operate on other instances of class fraction
         int local_numer = numerator;
         int local_denom = denominator;
 
@@ -66,25 +73,30 @@ public class FractionImpl implements Fraction {
             }
         }
 
-        // handling negatives: if num and denom negative, axe both signs
+        // handling negatives: if num and denom negative, remove both signs
         if (local_numer < 0 && local_denom < 0) {
             local_numer *= -1;
             local_denom *= -1;
-        } else if (local_numer < 0 ^ local_denom < 0) {
-            // handling negatives: If only one is negative, make numer negative and denom positive
+        }
+
+        // handling negatives: If only one is negative, make numer negative and denom positive
+        else if (local_numer < 0 ^ local_denom < 0) {
             if (local_numer > 0) local_numer *= -1;
             if (local_denom < 0) local_denom *= -1;
         }
+        
         int[] numer_denom3 = new int[2];
         numer_denom3[0] = local_numer;
         numer_denom3[1] = local_denom;
         return numer_denom3;
     }
 
-    private <T> int[] get_f_values(T obj) {
-        // return obj's numer, denom
-        // accepts whole number or Fraction object or String
-
+    /**
+     * internal use
+     * accepts whole number or Fraction object or String
+     * returns object F's numerator and denominator
+     */
+    private <F> int[] get_f_values(F obj) {
         String fstr = obj.toString();
         int[] f_num_denom = new int[2];
         if (fstr.contains("/")) {
@@ -96,6 +108,11 @@ public class FractionImpl implements Fraction {
         return f_num_denom;
     }
 
+    /**
+     * accepts fraction f
+     * adds current fraction to fraction f
+     * returns result as a new fraction
+     */
     @Override
     public Fraction add(Fraction f) {
         int fc = get_f_values(f)[0];
@@ -105,6 +122,11 @@ public class FractionImpl implements Fraction {
         return new FractionImpl(top, bot);
     }
 
+    /**
+     * accepts fraction f
+     * subtracts fraction f from current fraction
+     * returns result as a new fraction
+     */
     @Override
     public Fraction subtract(Fraction f) {
         int fc = get_f_values(f)[0];
@@ -114,6 +136,11 @@ public class FractionImpl implements Fraction {
         return new FractionImpl(top, bot);
     }
 
+    /**
+     * accepts fraction f
+     * multiplies current fraction with fraction f
+     * returns result as a new fraction
+     */
     @Override
     public Fraction multiply(Fraction f) {
         int top = this.numerator * get_f_values(f)[0];
@@ -121,6 +148,11 @@ public class FractionImpl implements Fraction {
         return new FractionImpl(top, bot);
     }
 
+    /**
+     * accepts fraction f
+     * divides current fraction by fraction f
+     * returns result as a new fraction
+     */
     @Override
     public Fraction divide(Fraction f) {
         int bot = this.denominator * get_f_values(f)[0];
@@ -128,21 +160,38 @@ public class FractionImpl implements Fraction {
         return new FractionImpl(top, bot);
     }
 
+    /**
+     * returns absolute value of current fraction
+     * takes no arguments
+     */
     @Override
     public Fraction abs() {
         return new FractionImpl(Math.abs(this.numerator), Math.abs(this.denominator));
     }
 
+    /**
+     * returns negated value of current fraction
+     * takes no arguments
+     */
     @Override
     public Fraction negate() {
         return new FractionImpl((this.numerator * -1), this.denominator);
     }
 
+    /**
+     * returns hashCode value of current fraction
+     * takes no arguments
+     */
     @Override
     public int hashCode() {
         return super.hashCode();
     }
 
+    /**
+     * compares the value current fraction to Object o
+     * returns true if o is a Fraction object of equal value to current fraction
+     * otherwise, returns false
+     * */
     @Override
     public boolean equals(Object o) {
         if (o instanceof Fraction) {
@@ -172,6 +221,7 @@ public class FractionImpl implements Fraction {
         return super.clone();
     }
 
+    /** returns the inverse of the current fraction */
     @Override
     public Fraction inverse() {
         // a/b --> b/a
@@ -184,23 +234,9 @@ public class FractionImpl implements Fraction {
         }
     }
 
-    // problem: in original FractionImpl it's public int compareTo(Fraction o)
-    // in original Fraction.java it's public int compareTo(Fraction o)
-    // in coursework instructions it's public int compareTo(Object o)
-    // todo: ask why that is / what to do.
-    // for now, I'm following Fraction.java's instructions instead of webpage's
-    // but that leaves me failing to throw ClassCastException as per webpage instructions
-//    @Override
-//    public int compareTo(Fraction o) {
-//        if (o instanceof Fraction || o instanceof Integer){
-//            double this_value = (double) this.numerator / (double) this.denominator;
-//            int[] o_vals = get_f_values(o);
-//            double o_value = (double) o_vals[0] / (double) o_vals[1];
-//            return Double.compare(this_value, o_value);
-//        }
-//        else throw new ClassCastException("compareTo function received neither fraction nor int");
-//    }
-
+    /** compares this fraction to Fraction o
+     * returns 1 if this fraction is greater than Fraction o, 0 if equal, -1 if lesser
+     */
     @Override
     public int compareTo(Fraction o) {
         double this_value = (double) this.numerator / (double) this.denominator;
@@ -209,7 +245,7 @@ public class FractionImpl implements Fraction {
         return Double.compare(this_value, o_value);
     }
 
-
+    /** returns the String object representation of this fraction */
     @Override
     public String toString() {
         if (this.denominator == 1) return Integer.toString(this.numerator);
